b3601d367ecde612831ccb5555018837ae0e5d74
Noor El-Din Walid <52334140+iNoor72@users.noreply.github.com>
2024-03-08 16:53:15 +0200

    Added movies endpoints

---
+++ TMDB-SwiftUI/Services/Network/Endpoints/MoviesEndpoint.swift
@@ -0,0 +1,72 @@
+//
+//  MoviesEndpoint.swift
+//  TMDB-SwiftUI
+//
+//  Created by Noor El-Din Walid on 08/03/2024.
+//
+
+import Foundation
+import Alamofire
+
+enum MoviesEndpoint {
+    case popularMovies
+    case movieDetails(movieID: String)
+}
+
+extension MoviesEndpoint: Endpoint {
+    var baseURL: URL {
+        guard let url = URL(string: APIConstants.baseURL) else { fatalError("Base URL isn't valid.") }
+        return url
+    }
+    
+    var requestURL: URL {
+        baseURL.appendingPathExtension(path)
+    }
+    
+    var path: String {
+        switch self {
+        case .movieDetails(let movieID):
+            return "/\(movieID)"
+            
+        case .popularMovies:
+            return APIConstants.Paths.popularMoviesPath
+        }
+    }
+    
+    var parameters: Alamofire.Parameters? {
+        return nil
+    }
+    
+    var method: Alamofire.HTTPMethod {
+        switch self {
+        case .movieDetails(_), .popularMovies:
+            return .get
+        }
+    }
+    
+    var headers: HTTPHeaders {
+        return  ["accept": "application/json",
+                 "Authorization": "Bearer \(APIConstants.token)"
+        ]
+    }
+    
+    var encoding: ParameterEncoding {
+        switch method {
+        case .post:
+            return JSONEncoding.default
+        default:
+            return URLEncoding.default
+        }
+    }
+    
+    var urlRequest: URLRequest {
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = method.rawValue
+        request.headers = headers
+        return request
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        try encoding.encode(urlRequest, with: parameters)
+    }
+}
--- TMDB-SwiftUI/Utilities/APIConstants.swift
+++ TMDB-SwiftUI/Utilities/APIConstants.swift
@@ -9,6 +9,7 @@ import Foundation
 
 enum APIConstants {
     static let baseURL = "https://api.themoviedb.org/3/movie"
+    static let token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDExZjNmZGM2NTUxZWJlNTQ3ZjE4MWM3OTY4MGI2NiIsInN1YiI6IjVmNGJlY2UzMTJhYWJjMDAzNGE3YzRjYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v78toPmKrx5CQcZEF79CiPQcCoZURpaFBl-P1-0xprs"
     
     enum Paths {
         static let popularMoviesPath = "/popular"

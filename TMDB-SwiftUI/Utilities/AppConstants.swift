df0207de175a99e23e243bf1d0c3dc52fe45e936
Noor El-Din Walid <52334140+iNoor72@users.noreply.github.com>
2024-03-08 16:22:49 +0200

    Laying out the foundation

---
+++ TMDB-SwiftUI/Utilities/AppConstants.swift
@@ -0,0 +1,12 @@
+//
+//  AppConstants.swift
+//  TMDB-SwiftUI
+//
+//  Created by Noor El-Din Walid on 08/03/2024.
+//
+
+import Foundation
+
+enum AppConstants {
+    static let APIKey = "9011f3fdc6551ebe547f181c79680b66"
+}
---
+++ TMDB-SwiftUI/Utilities/AppStrings.swift
@@ -0,0 +1,12 @@
+//
+//  AppStrings.swift
+//  TMDB-SwiftUI
+//
+//  Created by Noor El-Din Walid on 08/03/2024.
+//
+
+import Foundation
+
+enum AppStrings {
+    
+}

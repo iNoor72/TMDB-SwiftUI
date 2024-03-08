db71d90a5cf6ec84c71affc60579ade7dba8441d
Noor El-Din Walid <52334140+iNoor72@users.noreply.github.com>
2024-03-08 16:17:18 +0200

    Initial Commit

---
+++ TMDB-SwiftUI/TMDB_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_SwiftUIApp.swift
+//  TMDB-SwiftUI
+//
+//  Created by Noor El-Din Walid on 08/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
